f172f76858b69c3a134a5dec9900dc7165894c9c
Abhishek Peiris <abhishekpeiris9@gmail.com>
2021-03-30 23:17:39 +0530

    Initial commit

---
+++ src/main/java/com/lk/codefest/day04/CodefestEeDay04Application.java
@@ -0,0 +1,14 @@
+package com.lk.codefest.day04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefestEeDay04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefestEeDay04Application.class, args);
+	}
+
+}
